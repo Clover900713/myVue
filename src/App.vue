483c71a8f2e33cbe964f1a29f240d16ed9d53542
wangyanni <wangyanni@xiaomi.com>
2018-06-27 20:30:37 +0800

    父组件传数据

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+      <HeaderDiv :logo="logoMsg"></HeaderDiv>
   </div>
 </template>
 
 <script>
+import HeaderDiv from './components/header'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      logoMsg: 'fakeQQ'
+    }
+  },
+  components: {
+    HeaderDiv
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+      <div id="logo">{{logo}}</div>
+      <ul class="nav">
+          <li v-for="nav in navs" :key="nav.id">{{nav.li}}</li>
+      </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'headerDiv',
+  data () {
+    return {
+      navs: [
+        {li: '主页', id: 1},
+        {li: '日志', id: 2},
+        {li: '说说', id: 3},
+        {li: '相册', id: 4}
+      ]
+    }
+  },
+  props: ['logo']
+}
+</script>
